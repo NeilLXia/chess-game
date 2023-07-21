c88a305370b272282208e3486f587702a808d511
Neil Xia <neil.l.xia@gmail.com>
2023-07-21 16:42:13 -0700

    vercel deployment

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import { useState, useEffect, useRef } from "react";
 import * as ReactDOM from "react-dom/client";
-
 import "./css/app.css";
-
 import {
   UserContext,
   BoardContext,
