6cf16b45a45b3837fd0bad6832bf8f3eba1a2e1c
Neil Xia <neil.l.xia@gmail.com>
2023-08-11 02:14:46 -0700

    fix install command

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,3 @@
 cd /home/ubuntu/chess-game
 echo "Y" | sudo apt install libcurl4-openssl-dev libssl-dev
-sudo pip3 install -r requirements.txt -t dependencies/
-pip3 install django
+sudo pip3 install -r requirements.txt
