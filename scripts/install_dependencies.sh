aca38aebbf30bc9a90a064fadc4ea85247859ea2
Neil Xia <neil.l.xia@gmail.com>
2023-08-11 04:05:34 -0700

    change server to run on a virtual environment

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,7 @@
-cd /home/ubuntu/chess-game
-echo "Y" | sudo apt install libcurl4-openssl-dev libssl-dev
-sudo pip3 install -r requirements.txt
+# update os & install python3 and libpq-dev for postgresql
+sudo apt-get update
+sudo apt-get install -y python3 python3-dev python3-pip python3-venv libpq-dev
+pip install --user --upgrade virtualenv
+
+# delete prior files
+sudo rm -rf /home/ubuntu/chess-game
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,17 @@
+# kill existing processess
+sudo pkill -f runserver
 
+# setup virtual environment
+echo "Y" | sudo apt install python3.10-venv
+python3 -m venv venv
+source venv/bin/activate
+
+# install dependencies
 cd /home/ubuntu/chess-game
-python3 server/manage.py runserver 0.0.0.0:8000 > /dev/null 2> /dev/null < /dev/null &
+echo "Y" | sudo apt install libcurl4-openssl-dev libssl-dev
+sudo pip3 install -r requirements.txt
+
+# run server
+python3 manage.py makemigrations
+python3 manage.py migrate
+screen -d -m python3 manage.py runserver 0:8000
