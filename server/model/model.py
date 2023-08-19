3ba95976bac1bcd229d2a55915dbbc65ff30bf97
Neil Xia <neil.l.xia@gmail.com>
2023-08-19 13:43:09 -0700

    test new game path

--- server/model/model.py
+++ server/model/model.py
@@ -22,14 +22,14 @@ async def createTree(root_id):
     if treeDB != None:
         try:
             await treeDB['trees'].insert_one({
-                'root_id': root_id,
+                'root_id': 1,
                 'nodes': [{
                     'board_state': '1513141611141315' + '12' * 8 + '00' * 32 + '02' * 8 + '05030406010405',
                     'user_state': {'selection_1': None,
                                    'selection_2': None,
                                    'can_castle': {
-                                       'black': {0: True, 7: True},
-                                       'white': {56: True, 63: True},
+                                       'black': {'0': True, '7': True},
+                                       'white': {'56': True, '63': True},
                                    },
                                    'player_turn': 'white'},
                     'timer': {'white': {'minutes': 5, 'seconds': 0},
--- server/model/mongodb.py
+++ server/model/mongodb.py
@@ -1,17 +1,13 @@
 import os
 import certifi
 from pymongo import MongoClient, errors
-from dotenv import load_dotenv
 from secrets_manager import get_secret
-
-load_dotenv()
 
 
 def get_database():
     try:
         CONNECTION_STRING = 'mongodb+srv://chessadmin:{password}@chess-history-database.k4rbjbb.mongodb.net/'.format(
             password=get_secret()["MONGODB_PASSWORD"], tlsCAFile=certifi.where())
-        # , ssl_cert_reqs=ssl.CERT_NONE)
         client = MongoClient(CONNECTION_STRING)
         return client['chess-history-database']
     except errors:
